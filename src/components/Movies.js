f6dc11bfa69aacd3e4014f3251a130588e00a2b7
asmaali2 <asma.maalim7@gmail.com>
2023-11-25 13:53:27 +0300

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,6 +3,19 @@ import { movies } from "../data";
 
 function Movies() {
   return <div>{/*{code here}*/}</div>;
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <p>Genres: {movie.genres.join(', ')}</p>
+          <p>Time: {movie.time}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 
 export default Movies;
